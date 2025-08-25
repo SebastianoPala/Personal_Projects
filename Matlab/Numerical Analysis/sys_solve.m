1905c7b3916953c93a65ad36909eb6c5578fe739
SebastianoPala <Sebastianoxpala@hotmail.com>
2025-08-25 12:06:38 +0200

    Removing bad files

--- Matlab/Numerical Analysis/sys_solve.m
+++
@@ -1,9 +0,0 @@
-function x=sys_solve(A,b)
-    [L,U,c]=my_gauss(A,b);
-    %y=inf_solve(L,b);   
-    %x=sup_solve(U,y); 
-    for i=1:size(b,2)
-        x(:,i)= sup_solve(U,c(:,i)); %c è L^-1 * b, posso farlo se ho fatto gauss su b, altrimenti devo
-    end         
-    
-end
